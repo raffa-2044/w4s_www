64d5cf680b73219371c8f7f9b7eb4de69e22c546
raffa-2044 <p88836959@gmail.com>
2026-02-02 11:29:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Progetti PHP</title>
+</head>
+<body>
+    <h1>Elenco Progetti PHP</h1>
+    <ul>
+        <li><a href="ES01-HelloPhp/">ES01 - HelloPhp</a></li>
+        <li><a href="ES02-Variabili/">ES02 - Variabili</a></li>
+    aggiungere qui altri link</li>
+    </ul>
+
+    <?php
+    // Sezione phpinfo()
+    echo "<hr><h2>Informazioni PHP</h2>";
+    phpinfo();
+    ?>
+</body>
+</html>
